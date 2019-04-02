4b5b893f40341d2530c07fc6084c5a4f2c2f44ed
unknown <ruchita.gandhe@gmail.com>
2019-04-02 14:46:39 -0400

    linked homewok 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var computerGuess = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var computerGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 var wins = 0;
 var losses = 0;
@@ -11,19 +11,19 @@ var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessesText = document.getElementById("guesses-text");
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
   if (guesses > 0) {
 
     var userPick = event.key;
     console.log(userPick.toLowerCase());
 
-    var computerPick = computerGuess[Math.floor(Math.random()*computerGuess.length)]; 
+    var computerPick = computerGuess[Math.floor(Math.random() * computerGuess.length)];
     console.log(computerPick);
-    
+
     if (userPick === computerPick) {
       wins++
       guesses--
-    }else{
+    } else {
       guesses--
       losses++
     }
